b4fd0bcdb560db3690059578a9c90b615996f986
Telionps <93835565+Telionps@users.noreply.github.com>
2022-01-25 20:11:51 -0500

    Update ArteaCaseCodeLouis.R

--- ArteaCaseCodeLouis.R
+++ ArteaCaseCodeLouis.R
@@ -1,5 +1,7 @@
 ######Imports
 install.packages("readxl")
+install.packages("writexl")
+
 library("readxl")
 library(ggplot2)
 library(gridExtra)
@@ -7,6 +9,8 @@ library(dplyr)
 library(car)
 library(purrr)
 library(tidyverse)
+library("writexl")
+
 ##### Data Handling
 
 ABtest <- read_excel("521703-XLS-ENG.xlsx", sheet=2)
@@ -273,7 +277,6 @@ sum.trans.cart <- aggregate(trans_after ~ test_coupon + shopping_cart, ABtest, s
 avg.trans.cart <- aggregate(trans_after ~ test_coupon + shopping_cart, ABtest, mean)
 
 
-
 ##Testing if significant
 ABtest.cart <- subset(ABtest, shopping_cart==1)
 wilcox.test(revenue_after ~ test_coupon, data = ABtest.cart)
@@ -281,14 +284,28 @@ wilcox.test(revenue_after ~ test_coupon, data = ABtest.cart)
 
 
 ##### Minority
-sum.rev.minority <- aggregate(revenue_after ~ minority + test_coupon, ABtest, sum)
-
+sum.rev.minority <- aggregate(revenue_after ~ test_coupon + minority, ABtest, sum)
 #Mean of revenue for top_60% spenders who were given coupon vs not
-avg.rev.minority <- aggregate(revenue_after ~ minority + test_coupon, ABtest, mean)
+avg.rev.minority <- aggregate(revenue_after ~ test_coupon + minority, ABtest, mean)
+
+##Testing if significant
+ABtest.minority <- subset(ABtest, minority==1)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.minority)
+
+##### Female
+sum.rev.female <- aggregate(revenue_after ~ test_coupon + female, ABtest, sum)
+#Mean of revenue for top_60% spenders who were given coupon vs not
+avg.rev.female <- aggregate(revenue_after ~ test_coupon + female, ABtest, mean)
+
+##Testing if significant
+ABtest.female <- subset(ABtest, female==1)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.female)
+
+ABtest.male <- subset(ABtest, female==0)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.male)
 
 ##### Shopping_cart * channels
 sum.rev.cart_channels <- aggregate(revenue_after ~ test_coupon + shopping_cart + channel_acq, ABtest, sum)
-
 #Mean of revenue for top_60% spenders who were given coupon vs not
 avg.rev.cart_channels <- aggregate(revenue_after ~ test_coupon + shopping_cart + channel_acq, ABtest, mean)
 
@@ -412,19 +429,41 @@ gg.cart_trans_mean <- ggplot(avg.trans.cart, aes(fill=shopping_cart, y=trans_aft
 
 grid.arrange(gg.cart_rev, gg.cart_rev_mean, gg.cart_trans, gg.cart_trans_mean)
 
-### Plots for channel & cart
-ABtest %>%
-  mutate(text = fct_reorder(text, value)) %>%
-  ggplot( aes(x=value, color=text, fill=text)) +
-  geom_histogram(alpha=0.6, binwidth = 5) +
-  scale_fill_viridis(discrete=TRUE) +
-  scale_color_viridis(discrete=TRUE) +
-  theme_ipsum() +
-  theme(
-    legend.position="none",
-    panel.spacing = unit(0.1, "lines"),
-    strip.text.x = element_text(size = 8)
-  ) +
-  xlab("") +
-  ylab("Assigned Probability (%)") +
-  facet_wrap(~text)
+#### Big grid
+grid.arrange(gg.channel_rev, gg.channel_trans,
+             gg.cart_rev, gg.cart_trans,
+             ncol = 2)
+
+##### Table question E
+avg.rev.3$diff <- rep(avg.rev.3$revenue_after[seq(2,nrow(avg.rev.3),by=2)] - avg.rev.3$revenue_after[seq(1,nrow(avg.rev.3),by=2)], each=2)
+diff_table <- avg.rev.3[ !duplicated(avg.rev.3$diff), ]
+diff_table = diff_table[,-1]
+
+sum.rev.3$diff <- rep(sum.rev.3$revenue_after[seq(2,nrow(sum.rev.3),by=2)] - sum.rev.3$revenue_after[seq(1,nrow(sum.rev.3),by=2)], each=2)
+diff_table_sum <- sum.rev.3[ !duplicated(sum.rev.3$diff), ]
+diff_table_sum = diff_table_sum[,-1]
+
+diff_table_sum$diff_avg <- diff_table$diff
+
+write_xlsx(diff_table_sum,"C:\\Users\\louis\\OneDrive - McGill University\\MMA Winter 2021 Notes\\MRKT671\\Artea Case\\Data\\diff_table.xlsx")
+
+#Have identified 5 positive results greater than 5
+#1: cart = 1, female = 1, channel_acq = Facebook
+ABtest.signif <- subset(ABtest, channel_acq == "Facebook" & shopping_cart == 1 & female == 1)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.signif)
+
+#2: cart = 1, female = 1, channel_acq = Instragram
+ABtest.signif <- subset(ABtest, channel_acq == "Instragram" & shopping_cart == 1 & female == 1)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.signif)
+
+#3: cart = 1, female = 0, channel_acq = Referral
+ABtest.signif <- subset(ABtest, channel_acq == "Referral" & shopping_cart == 1 & female == 0)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.signif)
+
+#4: cart = 1, female = 1, channel_acq = Referral
+ABtest.signif <- subset(ABtest, channel_acq == "Referral" & shopping_cart == 1 & female == 1)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.signif)
+
+#5: cart = 1, female = 0, channel_acq = Other
+ABtest.signif <- subset(ABtest, channel_acq == "Other" & shopping_cart == 1 & female == 0)
+wilcox.test(revenue_after ~ test_coupon, data = ABtest.signif)
